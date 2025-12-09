4d7d0dedf1c86229de62cf6556b6ff640fa743b0
Ahmed Essam <ae.devverse@gmail.com>
2025-12-09 22:26:42 +0200

    Feat: Create ItemList component

---
+++ components/ItemList.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { Button } from "./ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import InvoiceItem from "./InvoiceItem";
+
+export const items = [
+  {
+    id: 1,
+    description: "Website Design",
+    quantity: 1,
+    rate: 500,
+    amount: 500,
+  },
+  {
+    id: 2,
+    description: "Hosting (12+ months)",
+    quantity: 1,
+    rate: 120,
+    amount: 120,
+  },
+];
+
+function ItemList() {
+  const addItem = () => {};
+
+  return (
+    <Card>
+      <CardHeader className="flex items-center justify-between">
+        <CardTitle>Invoice Items</CardTitle>
+
+        <Button onClick={addItem} size="sm" className="w-fit cursor-pointer">
+          Add Item
+          <Plus className="w-4 h-4 me-2" />
+        </Button>
+      </CardHeader>
+
+      <CardContent className="space-y-4">
+        {items.map((item, index) => (
+          <InvoiceItem
+            key={item.id}
+            item={item}
+            index={index}
+            canRemove={items.length > 1}
+          />
+        ))}
+      </CardContent>
+    </Card>
+  );
+}
+
+export default ItemList;
